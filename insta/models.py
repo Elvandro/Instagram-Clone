b66b565e21cfca68722c315f0d235f465436d318
Elvandro <elvisamuni22@gmail.com>
2018-07-24 16:51:05 +0300

    Created the comment and friend models

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -29,6 +30,7 @@ class Image(models.Model):
     profile_key = models.ForeignKey(Profile,on_delete=models.CASCADE)
     user_key = models.ForeignKey(User,on_delete= models.CASCADE)
     likes = models.PositiveIntegerField(default=0)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.image_name
@@ -52,3 +54,38 @@ class Image(models.Model):
     def get_images_by_user(cls,id):
         posted_images = Image.objects.filter(user_id=id)
         return posted_images
+
+class Comment(models.Model):
+    comment = models.TextField(blank=True)
+    date_posted = models.DateTimeField(auto_now_add=True, null=True)
+    Creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.comment
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    @classmethod
+    def get_comments(cls, image_id):
+        comment = Comment.objects.filter(image=image_id).all().order_by('-date_posted')
+        return comment
+
+class Friend(models.Model):
+    users = models.ManyToManyField(User)
+    current_user = models.ForeignKey(User, related_name='owner', null=True)
+
+    @classmethod
+    def make_friend(cls, current_user, new_friend):
+        friend, created = cls.objects.get_or_create(current_user=current_user)
+        friend.users.add(new_friend)
+
+    @classmethod
+    def lose_friend(cls, current_user, new_friend):
+        friend, created = cls.objects.get_or_create(current_user=current_user)
+        friend.users.remove(new_friend)
