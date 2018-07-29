252f8c4f6d0bd013396fc3ef718a2d87457bfd68
Elvandro <elvisamuni22@gmail.com>
2018-07-29 23:07:05 +0300

    Created urls

--- clone/urls.py
+++ clone/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('insta.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^tinymce/', include('tinymce.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^tinymce/', include('tinymce.urls')),
     url(r'^friendship/', include('friendship.urls')),
 ]
