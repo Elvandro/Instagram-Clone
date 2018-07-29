e5df143b9b49b5a6931fc702612ed809b9b4b3c5
Elvandro <elvisamuni22@gmail.com>
2018-07-29 23:10:23 +0300

    Created the views functions

--- insta/views.py
+++ insta/views.py
@@ -1,102 +1,128 @@
-from django.http  import Http404
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db import transaction
 from django.contrib.auth.models import User
 from . models import Profile, Image, Comment
+# from django.http  import Http404
 from . forms import ImageUploadForm, ProfileUpdateForm, CommentForm, CaptionUpdateForm
 from friendship.models import Friend, Follow, Block
-from django.contrib.auth.decorators import login_required
 import datetime as dt
 
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
-    date = dt.date.today()
-    friends = Friend.objects.friends(request.user)
-
     current_user = request.user
-    return render(request, 'index.html')
-
-
-@login_required(login_url='/accounts/login/')
-def profile(request, profile_name):
-    user_id = request.user.pk
-    user = User.objects.get(username = username)
-    profile = Profile.objects.get(user=user)
-    images = Image.objects.filter(user_id = user)
-    image_count = Image.objects.filter(user_id = user)
-    number_of_friends = Friend.objects.friends(request.user)
-
-    title = 'My Profile'
-    return render(request, 'profile.html', {"title":title, "user":user, "images":images, "image_count":image_count, "profile":profile, "friends":number_of_friends})
-
+    images = Image.objects.all()
+    profiles = Profile.objects.all()
+    user = Profile.objects.filter(user=current_user)
+    content = {
+        "current_user": current_user,
+        "images": images,
+        "profiles":profiles,
+        "user": user
+    }
+    return render(request, 'index.html', content)
 
 @login_required(login_url='/accounts/login/')
-def update_profile(request):
+def post(request):
     current_user = request.user
-    try:
-        requested_profile = Profile.objects.get(user_id = current_user.id)
-        if request.method == 'POST':
-            form = ProfileUpdateForm(request.POST,request.FILES)
-
-            if form.is_valid():
-                requested_profile.profile_photo = form.cleaned_data['profile_photo']
-                requested_profile.bio = form.cleaned_data['bio']
-                requested_profile.profile_name = form.cleaned_data['profile_name']
-                requested_profile.save_profile()
-                return redirect(profile)
-        else:
-            form = ProfileUpdateForm()
-    except:
-        if request.method == 'POST':
-            form = ProfileUpdateForm(request.POST,request.FILES)
-
-            if form.is_valid():
-                new_profile = Profile(profile_photo= form.cleaned_data['profile_photo'],bio = form.cleaned_data['bio'],profile_name = form.cleaned_data['profile_name'],user = current_user)
-                new_profile.save_profile()
-                return redirect(profile)
-        else:
-            form = ProfileUpdateForm()
-
-
-    title = 'Update Profile'
-    return render(request,'update_profile.html',{"title":title,"current_user":current_user,"form":form})
-
-
+    profiles = Profile.objects.all()
+    form = ImageUploadForm()
+    for profile in profiles:
+        if profile.user.id == current_user.id:
+            if request.method == 'PSOT':
+                form = ImageUploadForm(request.POST, request.FILES)
+                if form.is_valid():
+                    post = form.save(commit=False)
+                    post.user_key = current_user
+                    post.profile = profile
+                    post.save()
+                    return redirect('index')
+            else:
+                form = ImageUploadForm()
+    return render(request, 'post.html', {"post_form":form, "user": current_user})
 
 @login_required(login_url='/accounts/login/')
-def post(request, profile_name):
-    user = User.objects.get(profile_name = profile_name)
+def comment(request, pk):
+    image = get_object_or_404(Image, pk=pk)
     current_user = request.user
     if request.method == 'POST':
-        form = ImageUploadForm(request.POST, files=request.FILES)
+        form = CommentForm(request.POST)
         if form.is_valid():
-            image = form.save(commit=False)
-            image.user = current_user
-            image.save()
-
-        return redirect(profile, current_user.profile_name)
+            comment = form.save(commit=False)
+            comment.image = image
+            comment.creator = current_user
+            comment.save()
+            return redirect('index')
     else:
-        form = ImageUploadForm()
+        form = CommentForm()
 
-    return render(request, 'post.html', {"form":form})
+    content = {
+        "comment_form": form,
+    }
+    return render(request, 'comment.html', content)
+
+@login_required(login_url='/accounts/login/')
+def search_result(request):
+    if 'query' in request.GET and request.GET['query']:
+        query = request.GET.get("query")
+        user = Profile.find_profile(query)
+        images = Image.objects.all()
+        message = f"query"
+
+        content = {
+            "message": message,
+            "found": user,
+            "images": images,
+        }
+
+        return render(request, 'search.html', content)
+    else:
+        message = "You haven't searched for anyone"
+        return render(request, 'search.html', {"message": message})
 
 
 @login_required(login_url='/accounts/login/')
-def comment(request, image_id):
-    comments = Comment.objects.filter(image_id=image_id)
-    current_image = Image.objects.get(image_id=image_id)
+def like(request, operation, pk):
+    image = image = get_object_or_404(Image, pk=pk)
+    if operation == 'like':
+        image.likes = image.likes + 1
+        image.save()
+    elif operation == 'unlike':
+        image.likes = image.likes - 1
+        image.save()
+    return redirect('index')
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
     current_user = request.user
+    images = Image.objects.filter(user_key=request.user)
+    profiles = Profile.objects.filter(user=request.user)
+    content = {
+        "current_user": current_user,
+        "images": images,
+        "profiles": profiles
+    }
+    return render(request, 'profile.html', content)
+
+
+@login_required(login_url='/accounts/login/')
+@transaction.atomic
+def add_profile(request):
+    current_user = request.user
+    user_profile = Profile.objects.filter(user_id=current_user)
     if request.method == 'POST':
-        comment_form = CommentForm(request.POST)
-        if comment_form.is_valid():
-            comment = comment_form.save(commit = False)
-            comment.user_id= current_user
-            comment.image_id = current_image
-            comment.save()
-
-            return redirect(index)
+        form = ProfileUpdateForm(request.POST, request.FILES)
+        if form.is_valid():
+            user_profile = form.save(commit=False)
+            user_profile.user = current_user
+            user_profile.save()
+            return redirect('index')
     else:
-        comment_form = CommentForm()
+        form = ProfileUpdateForm(instance=request.user)
 
-    return render(request, 'comment.html', {"form":comment_form,"comments":comments})
+        content = {
+            "form": form,
+        }
+        return render(request, 'update-profile.html', content)
