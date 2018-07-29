c2876e42a827b23be329f9541f805d91d125056b
Elvandro <elvisamuni22@gmail.com>
2018-07-29 23:10:01 +0300

    Edited the Urls

--- insta/urls.py
+++ insta/urls.py
@@ -4,7 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$', views.index, name = 'index')
+    url(r'^$', views.index, name='index'),
+    url(r'^post/', views.post, name='post'),
+    url(r'^comment/(?P<pk>\d+)', views.comment, name='comment'),
+    url(r'^search/', views.search_result, name='search'),
+    url(r'^like/(?P<operation>.+)/(?P<pk>\d+)', views.like, name='like'),
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^$', views.add_profile, name='edit'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
